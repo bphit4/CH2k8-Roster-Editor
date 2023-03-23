b9022e84780b27cf9c2b8f9de777e14c24abcd90
BPhit4 <72659934+bphit4@users.noreply.github.com>
2023-03-23 18:40:13 -0400

    Add files via upload

---
+++ ui_functions.py
@@ -0,0 +1,148 @@
+import sys
+import struct
+import os
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QTableWidget,
+                             QTableWidgetItem, QMenuBar, QMenu, QAction,
+                             QFileDialog, QMessageBox, QLabel, QUndoStack, QUndoCommand)
+from PyQt5.QtCore import Qt
+import chardet
+import csv
+
+class CustomTableWidget(QTableWidget):
+    def __init__(self, *args, **kwargs):
+        super(CustomTableWidget, self).__init__(*args, **kwargs)
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.context_menu)
+        self.clipboard_data = []
+
+    def context_menu(self, pos):
+        menu = QMenu()
+
+        undo_action = menu.addAction("Undo")
+        redo_action = menu.addAction("Redo")
+        menu.addSeparator()
+        cut_action = menu.addAction("Cut")
+        copy_action = menu.addAction("Copy")
+        paste_action = menu.addAction("Paste")
+        delete_action = menu.addAction("Delete")
+        menu.addSeparator()
+        select_all_action = menu.addAction("Select All")
+        menu.addSeparator()
+        hide_columns_action = menu.addAction("Hide Columns")
+        show_all_columns_action = menu.addAction("Show All Columns")
+
+        action = menu.exec_(self.viewport().mapToGlobal(pos))
+
+        if action == undo_action:
+            self.undo()
+        elif action == redo_action:
+            self.redo()
+        elif action == cut_action:
+            self.cut()
+        elif action == copy_action:
+            self.copy()
+        elif action == paste_action:
+            self.paste()
+        elif action == delete_action:
+            self.delete()
+        elif action == select_all_action:
+            self.selectAll()
+        elif action == hide_columns_action:
+            self.hideColumns()
+        elif action == show_all_columns_action:
+            self.showAllColumns()
+
+    def undo(self):
+        self.parent().undo_stack.undo()
+
+    def redo(self):
+        self.parent().undo_stack.redo()
+
+    def cut(self):
+        self.copy()
+        self.delete()
+
+    def copy(self):
+        self.clipboard_data.clear()
+        for cell_range in self.selectedRanges():
+            for row in range(cell_range.topRow(), cell_range.bottomRow() + 1):
+                for col in range(cell_range.leftColumn(), cell_range.rightColumn() + 1):
+                    item = self.item(row, col)
+                    if item:
+                        self.clipboard_data.append(((row, col), item.text()))
+
+    def paste(self):
+        if self.clipboard_data:
+            row_offset = self.currentRow() - self.clipboard_data[0][0][0]
+            col_offset = self.currentColumn() - self.clipboard_data[0][0][1]
+            for (row, col), text in self.clipboard_data:
+                new_row = row + row_offset
+                new_col = col + col_offset
+                if 0 <= new_row < self.rowCount() and 0 <= new_col < self.columnCount():
+                    self.setItem(new_row, new_col, QTableWidgetItem(text))
+
+    def delete(self):
+        for cell_range in self.selectedRanges():
+            for row in range(cell_range.topRow(), cell_range.bottomRow() + 1):
+                for col in range(cell_range.leftColumn(), cell_range.rightColumn() + 1):
+                    self.setItem(row, col, QTableWidgetItem(""))
+
+    def hideColumns(self):
+        selected_columns = set()
+        for cell_range in self.selectedRanges():
+            for col in range(cell_range.leftColumn(), cell_range.rightColumn() + 1):
+                selected_columns.add(col)
+        for col in selected_columns:
+            self.setColumnHidden(col, True)
+
+    def showAllColumns(self):
+        for col in range(self.columnCount()):
+            self.setColumnHidden(col, False)
+
+class EditCommand(QUndoCommand):
+    def __init__(self, table, row, col, old_value, new_value):
+        super().__init__()
+        self.table = table
+        self.row = row
+        self.col = col
+        self.old_value = old_value
+        self.new_value = new_value
+
+    def undo(self):
+        self.table.item(self.row, self.col).setText(self.old_value)
+
+    def redo(self):
+        self.table.item(self.row, self.col).setText(self.new_value)
+
+class EditCommand(QUndoCommand):
+    def __init__(self, table, row, column, old_value, new_value):
+        super().__init__()
+        self.table = table
+        self.row = row
+        self.column = column
+        self.old_value = old_value
+        self.new_value = new_value
+
+    def undo(self):
+        self.table.item(self.row, self.column).setText(self.old_value)
+
+    def redo(self):
+        self.table.item(self.row, self.column).setText(self.new_value)
+
+class MultiEditCommand(QUndoCommand):
+    def __init__(self, table, changes):
+        super().__init__()
+        self.table = table
+        self.changes = changes
+
+    def undo(self):
+        for row, col, old_value, _ in self.changes:
+            item = QTableWidgetItem(old_value)
+            item.setData(Qt.UserRole, old_value)
+            self.table.setItem(row, col, item)
+
+    def redo(self):
+        for row, col, _, new_value in self.changes:
+            item = QTableWidgetItem(new_value)
+            item.setData(Qt.UserRole, new_value)
+            self.table.setItem(row, col, item)
